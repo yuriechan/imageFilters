f0b90128329ac32a3a2aeddefb934ec5c58d16bd
Yurie <cutecupcakeandcute@gmail.com>
2020-05-26 17:37:56 +0900

    add sepia filter

--- filters.py
+++ filters.py
@@ -23,6 +23,7 @@ class Application:
         self.no_filter_btn = Button(master, text="Original", command=self.no_filter).place(relx="0.2", rely="0.9", anchor=N)
         self.code_in_place_filter_btn = Button(master, text="CodeInPlace", command=self.code_in_place_filter).place(relx="0.3", rely="0.9", anchor=N)
         self.grayscale_filter_btn = Button(master, text="Gray Scale", command=self.gray_scale_filter).place(relx="0.4", rely="0.9", anchor=N)
+        self.sepia_filter_btn = Button(master, text="Sepia", command=self.sepia_filter).place(relx="0.5", rely="0.9", anchor=N)
         self.save_filtered_image_btn = Button(master, text="Save", command=self.save_to_repo).place(relx="0.6", anchor=N)        
 
     def open_finder(self):
@@ -42,6 +43,24 @@ class Application:
         new_height = int(float(image_height) * float(width_percent))
         return image.resize((IMAGE_WIDTH, new_height), Image.ANTIALIAS)
 
+    def sepia_filter(self):
+        copied_image = self.resized_image.copy()
+        pixels = np.array(copied_image).astype(np.float)
+        for x in range(copied_image.height):
+            for y in range(copied_image.width):
+                sepiaRBG = [None] * 3
+                sepiaRBG[0] = self.clamp(round(.393 * pixels[x, y, R] + .769 * pixels[x, y, G] + .189 * pixels[x, y, B]))
+                sepiaRBG[1] = self.clamp(round(.349 * pixels[x, y, R] + .686 * pixels[x, y, G] + .168 * pixels[x, y, B]))
+                sepiaRBG[2] = self.clamp(round(.272 * pixels[x, y, R] + .534 * pixels[x, y, G] + .131 * pixels[x, y, B]))
+
+                pixels[x, y, R] = sepiaRBG[0]
+                pixels[x, y, G] = sepiaRBG[1]
+                pixels[x, y, B] = sepiaRBG[2]
+        
+        self.filtered_image = Image.fromarray(pixels.astype(np.uint8))
+        self.replace_image_in_widget(self.filtered_image)
+
+
     def gray_scale_filter(self):
         copied_image = self.resized_image.copy()
         pixels = np.array(copied_image).astype(np.float)
@@ -52,7 +71,7 @@ class Application:
                 total += pixels[x, y, G]
                 total += pixels[x, y, B]
 
-                ave = round(total / 3)
+                ave = self.clamp(round(total / 3))
                 pixels[x, y, R] = ave
                 pixels[x, y, G] = ave
                 pixels[x, y, B] = ave
